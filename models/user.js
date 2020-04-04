8900af1d3e455dc1013323bbdb146195503001fd
Julianna Polner <42859990+jj135969@users.noreply.github.com>
2020-04-04 00:08:43 -0700

    Passport Facebook

--- models/user.js
+++ models/user.js
@@ -3,10 +3,20 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    facebookId: String,
     admin: {
         type: Boolean,
         default: false
     }
+
 });
 
 userSchema.plugin(passportLocalMongoose);
